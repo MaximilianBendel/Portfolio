7b7d2e30aa8fe83a002c09b6d0f1a6d0b5632e26
MaximilianBendel <max.bendel1996@gmail.com>
2024-08-24 13:47:51 +0200

    first parts from My Skills Section

--- src/app/main-content/about-me/about-me.component.ts
+++ src/app/main-content/about-me/about-me.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-about-me',
@@ -7,6 +7,23 @@ import { Component } from '@angular/core';
   templateUrl: './about-me.component.html',
   styleUrl: './about-me.component.scss'
 })
-export class AboutMeComponent {
+export class AboutMeComponent implements AfterViewInit {
+
+  @ViewChild('aboutMeBox') aboutMeBox!: ElementRef;
+
+  constructor() {}
+
+  ngAfterViewInit(): void {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          // Wenn das Element sichtbar wird, füge die Klasse 'visible' hinzu
+          this.aboutMeBox.nativeElement.classList.add('visible');
+        }
+      });
+    });
+
+    observer.observe(this.aboutMeBox.nativeElement);
+  }
 
 }
--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { AboveTheFoldComponent } from './above-the-fold/above-the-fold.component';
 import { AboutMeComponent } from './about-me/about-me.component';
+import { MySkillsComponent } from "./my-skills/my-skills.component";
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [AboveTheFoldComponent, AboutMeComponent],
+  imports: [AboveTheFoldComponent, AboutMeComponent, MySkillsComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
