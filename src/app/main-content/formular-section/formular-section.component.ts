b95b648471a922feb9d0841135f8b13f25041f79
MaximilianBendel <max.bendel1996@gmail.com>
2024-09-02 15:53:40 +0200

    added formular

--- src/app/main-content/formular-section/formular-section.component.ts
+++ src/app/main-content/formular-section/formular-section.component.ts
@@ -1,12 +1,35 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-formular-section',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './formular-section.component.html',
   styleUrl: './formular-section.component.scss'
 })
 export class FormularSectionComponent {
 
+  contactData = {
+    name: "",
+    email: "",
+    message: ""
+  }
+
+  onSubmit(ngForm: NgForm) {
+    if (ngForm.valid && ngForm.submitted) {
+      console.log(this.contactData);
+    this.contactData.name = "";
+    this.contactData.email = "";
+    this.contactData.message = "";
+    }
+  }
+  
+  isNameFocused = false;
+  isEmailFocused = false;
+  isMessageFocused = false;
+
 }
+
+
